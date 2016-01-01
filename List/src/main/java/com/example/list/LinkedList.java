158e5c8f5cd9b24508e07694352ecc9c84ff0d3f
Venkatesh Iyer <viyer@yahoo-inc.com>
2016-01-01 11:06:52 -0800

    first

---
+++ List/src/main/java/com/example/list/LinkedList.java
@@ -0,0 +1,336 @@
+package com.example.list;
+
+public class LinkedList {
+
+  // Internal helper class for a node
+  public static class ListNode {
+    public final int data;
+    public ListNode next;
+
+    public ListNode(int data) {
+      this.data = data;
+    }
+  }
+
+  public ListNode head;
+  public ListNode end;
+
+  public LinkedList() {
+    head = null;
+    end = null;
+  }
+
+  public void insert(int data) {
+    ListNode node = new ListNode(data);
+    if (head == null) {
+      head = node;
+      head.next = null;
+      end = head;
+    } else {
+      end.next = node;
+      end = end.next;
+      end.next = null;
+    }
+  }
+
+  public void delete(ListNode node) {
+    if (isEmpty()) {
+      return;
+    }
+
+    // First element of the list
+    if (node == head) {
+      head = head.next;
+      return;
+    }
+
+    // Intermediate and last element
+    ListNode temp = head;
+    while (temp != null) {
+      if (temp.next != null && node.data == temp.next.data) {
+        temp.next = temp.next.next;
+        return;
+      } else {
+        temp = temp.next;
+      }
+    }
+  }
+
+  public boolean isEmpty() {
+    return head == null;
+  }
+
+  /**
+   * <pre>
+   * Given a singly linked list, write a function to swap elements pairwise. For example, if the linked
+   * list is 1->2->3->4->5->6->7 then the function should change it to 2->1->4->3->6->5->7, and if the
+   * linked list is 1->2->3->4->5->6 then the function should change it to 2->1->4->3->6->5
+   * </pre>
+   * 
+   */
+  public void pairWiseSwap() {
+    if (head == null || head.next == null) {
+      return;
+    }
+    // In above example, previous points to (->) 1, current-> 2 and head->1
+    ListNode previous = head;
+    ListNode current = head.next;
+
+    // Update head of the new linked list, head->2
+    head = current;
+
+    while (true) {
+      // next-> 3
+      ListNode next = current.next;
+      // Make 2's next point to 1 and so on...
+      current.next = previous;
+
+      // This is the end of the linked list condition. next.next == null when there are odd numbers,
+      // next == null when there are even number of nodes in the linked list.
+      if (next == null || next.next == null) {
+        // Set 5's next to 7 when there's 7 in the list. If it contains only upto 6 nodes, then set
+        // 5's next to null, and break since the linked list has ended.
+        previous.next = next;
+        break;
+      }
+
+      // Update 1's next to point to 4 and so on...
+      previous.next = next.next;
+
+      // When we come here, 2's next will be pointing to 1, and 1's next will be pointing to 4. To
+      // continue, set previous->3 and current->4 and so on...
+      previous = next;
+      current = previous.next;
+    }
+  }
+
+  /**
+   * <pre>
+   * Given a linked list, reverse alternate nodes and append them to end of list. Extra allowed space is O(1)
+   * Examples -
+   * 
+   * Input List:  1->2->3->4->5->6
+   * Output List: 1->3->5->6->4->2
+   * 
+   * Input List:  12->14->16->18->20
+   * Output List: 12->16->20->18->14
+   * </pre>
+   */
+  public void reverseAlternateAndAppend() {
+    // Make odd point to the head odd->1
+    ListNode odd = head;
+    // If there are less than 3 nodes in the linked list, nothing needs to be done
+    if (odd == null || odd.next == null || odd.next.next == null) {
+      return;
+    }
+    // Make even point to one node ahead of odd. even->2
+    ListNode even = odd.next;
+    // Make 1's next point to 3. 1->3
+    odd.next = even.next;
+    // Make 2's next point to null, since this is the last element of the final list 2->null
+    even.next = null;
+    // Make odd point to 3 here. odd->3
+    odd = odd.next;
+    // Two pointers to track even and odd nodes
+    ListNode oddTracker = null;
+    ListNode evenTracker = null;
+
+    while (true) {
+      // oddTracker points to next odd node. oddTracker->5
+      oddTracker = odd.next.next;
+      // evenTracker points to next even node. evenTracker->4
+      evenTracker = odd.next;
+      // Make 4's next point to 2. 4->2
+      evenTracker.next = even;
+      // Update even to point to next even node. even->4
+      even = evenTracker;
+      if (oddTracker == null) {
+        // This is the end of linked list when there are even number of nodes. For example, when odd
+        // points to 5 (odd->5), then oddTracker will point to null. That's when we set 6's next to
+        // 4, 6->4 and we break out of the loop
+        odd.next = even;
+        break;
+      } else {
+        // Increment odd to point to next odd element, otherwise.
+        odd.next = oddTracker;
+        if (oddTracker.next == null) {
+          // Exit condition for odd number of nodes. For example, if we have 7 also, oddTracker
+          // points to 7, and 7's next is null. Then make 7's next point to 6. Then break.
+          oddTracker.next = even;
+          break;
+        }
+      }
+      odd = oddTracker;
+    }
+  }
+
+  /**
+   * Merge alternate nodes in a linked list.
+   * 
+   * <pre>
+   * List1 : 1->2, List2 : 3->4->5, then result : 1->3->2->4->5
+   * List1 : 1->2->3, List2: 4->5, then result : 1->4->2->5->3 and make List2=null
+   * List1 : 1->2, List2: 3->4, then result : 1->3->2->4
+   * </pre>
+   * 
+   * @param secondList
+   */
+  public void mergeAlternate(LinkedList secondList) {
+    ListNode first = head;
+    LinkedList second = secondList;
+    ListNode firstCurrent = first, secondCurrent = second.head;
+    ListNode firstNext = null;
+    ListNode secondNext = null;
+
+    while (firstCurrent.next != null && secondCurrent.next != null) {
+      // Keep track of next elements in each linked list
+      firstNext = firstCurrent.next;
+      secondNext = secondCurrent.next;
+
+      // Assign pointers to each other
+      firstCurrent.next = secondCurrent;
+      secondCurrent.next = firstNext;
+
+      // Increment
+      firstCurrent = firstNext;
+      secondCurrent = secondNext;
+    }
+
+    if (firstCurrent.next == null && secondCurrent.next != null) {
+      // Second list is longer, so assign next of first list's last node to second list
+      firstCurrent.next = secondCurrent;
+    } else if (secondCurrent.next == null && firstCurrent.next != null) {
+      // First list is longer, so assign next of second list's last element to next element in first
+      // list
+      secondCurrent.next = firstCurrent.next;
+      // Update first's next to last element in second list
+      firstCurrent.next = secondCurrent;
+      // Set second list's head to null
+      secondList.head = null;
+    } else {
+      // If both lists are of same length, connect first list's last element to second list's last
+      // element
+      firstCurrent.next = secondCurrent;
+    }
+  }
+
+  /**
+   * Detect if the linked list has a loop
+   * 
+   * @return
+   */
+  public boolean hasLoop() {
+    ListNode slow = head;
+    ListNode fast = head;
+    while (slow != null && fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (slow == fast) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Detect and remove the loop in the linked list
+   */
+  public void removeLoop() {
+    ListNode slow = head;
+    ListNode fast = head;
+    while (slow != null && fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      // Detect the loop
+      if (slow == fast) {
+        // Set slow to the head of the linked list
+        slow = head;
+        // Node to track previous of the fast node. Fast node points to some node inside the loop
+        ListNode fastPrevious = null;
+        // Slow points to head and fast points to some node in the loop, now if we increment both
+        // one place at a time, they will meet at the beginning of the loop
+        while (slow != fast) {
+          slow = slow.next;
+          // fastPrevious point to the previous node of the fast node
+          fastPrevious = fast;
+          fast = fast.next;
+        }
+        // Set the next of fastPrevious to null so that the linked list ends
+        fastPrevious.next = null;
+      }
+    }
+  }
+
+  /**
+   * Intersection of two sorted linked lists
+   * 
+   * @param first
+   * @param second
+   * @param result
+   */
+  public void intersection(ListNode first, ListNode second, LinkedList result) {
+    if (first == null || second == null) {
+      return;
+    }
+    if (first.data < second.data) {
+      intersection(first.next, second, result);
+    } else if (first.data > second.data) {
+      intersection(first, second.next, result);
+    } else {
+      result.insert(first.data);
+      intersection(first.next, second.next, result);
+    }
+  }
+
+  /**
+   * Union of two sorted linked lists
+   * 
+   * @param first
+   * @param second
+   * @param result
+   */
+  public void union(ListNode first, ListNode second, LinkedList result) {
+    if (first == null && second == null) {
+      return;
+    }
+    if (first != null && second == null) {
+      result.insert(first.data);
+      union(first.next, second, result);
+    } else if (first == null && second != null) {
+      result.insert(second.data);
+      union(first, second.next, result);
+    } else {
+      if (first.data < second.data) {
+        result.insert(first.data);
+        union(first.next, second, result);
+      } else if (first.data > second.data) {
+        result.insert(second.data);
+        union(first, second.next, result);
+      } else {
+        // If both values are same
+        result.insert(first.data);
+        union(first.next, second.next, result);
+      }
+    }
+  }
+
+  public ListNode reverseKElements(ListNode head, int k) {
+    ListNode current = head;
+    ListNode prev = null;
+    ListNode next = null;
+    int count = 0;
+    while (current != null && count < k) {
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+      count++;
+    }
+
+    if (next != null)
+      head.next = reverseKElements(next, k);
+
+    return prev;
+  }
+
+}
---
+++ List/src/test/java/com/example/list/LinkedListTest.java
@@ -0,0 +1,245 @@
+package com.example.list;
+
+import junit.framework.Assert;
+import com.example.list.LinkedList.ListNode;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LinkedListTest {
+
+  private LinkedList ll;
+
+  @Before
+  public void setup() {
+    ll = new LinkedList();
+    ll.insert(1);
+    ll.insert(2);
+    ll.insert(3);
+    ll.insert(4);
+    ll.insert(5);
+    ll.insert(6);
+  }
+
+  @Test
+  public void testInsert() {
+    ListNode temp = ll.head;
+    int i = 1;
+    while (temp != null) {
+      Assert.assertEquals(i, temp.data);
+      temp = temp.next;
+      i++;
+    }
+  }
+
+  @Test
+  public void testDelete() {
+    // Delete head node
+    ListNode head = ll.head;
+    ll.delete(head);
+    ListNode temp = ll.head;
+    int i = 2;
+    while (temp != null) {
+      Assert.assertEquals(i, temp.data);
+      temp = temp.next;
+      i++;
+    }
+
+    // Delete intermediate node
+    ListNode node = new ListNode(4);
+    ll.delete(node);
+    Assert.assertEquals(ll.head.data, 2);
+    Assert.assertEquals(ll.head.next.data, 3);
+    Assert.assertEquals(ll.head.next.next.data, 5);
+    Assert.assertEquals(ll.head.next.next.next.data, 6);
+
+    // Delete last node
+    node = new ListNode(6);
+    ll.delete(node);
+    Assert.assertEquals(ll.head.data, 2);
+    Assert.assertEquals(ll.head.next.data, 3);
+    Assert.assertEquals(ll.head.next.next.data, 5);
+    Assert.assertNull(ll.head.next.next.next);
+  }
+
+  @Test
+  public void testPairWiseSwapEvenNodes() {
+    ll.pairWiseSwap();
+    Assert.assertEquals(ll.head.data, 2);
+    Assert.assertEquals(ll.head.next.data, 1);
+    Assert.assertEquals(ll.head.next.next.data, 4);
+    Assert.assertEquals(ll.head.next.next.next.data, 3);
+    Assert.assertEquals(ll.head.next.next.next.next.data, 6);
+    Assert.assertEquals(ll.head.next.next.next.next.next.data, 5);
+  }
+
+  @Test
+  public void testPairWiseSwapOddNodes() {
+    ll.insert(7);
+    ll.pairWiseSwap();
+    Assert.assertEquals(ll.head.data, 2);
+    Assert.assertEquals(ll.head.next.data, 1);
+    Assert.assertEquals(ll.head.next.next.data, 4);
+    Assert.assertEquals(ll.head.next.next.next.data, 3);
+    Assert.assertEquals(ll.head.next.next.next.next.data, 6);
+    Assert.assertEquals(ll.head.next.next.next.next.next.data, 5);
+    Assert.assertEquals(ll.head.next.next.next.next.next.next.data, 7);
+  }
+
+  @Test
+  public void testReverseAlternateAndAppendEvenNodes() {
+    ll.reverseAlternateAndAppend();
+    Assert.assertEquals(ll.head.data, 1);
+    Assert.assertEquals(ll.head.next.data, 3);
+    Assert.assertEquals(ll.head.next.next.data, 5);
+    Assert.assertEquals(ll.head.next.next.next.data, 6);
+    Assert.assertEquals(ll.head.next.next.next.next.data, 4);
+    Assert.assertEquals(ll.head.next.next.next.next.next.data, 2);
+  }
+
+  @Test
+  public void testReverseAlternateAndAppendOddNodes() {
+    ll.insert(7);
+    ll.reverseAlternateAndAppend();
+    Assert.assertEquals(ll.head.data, 1);
+    Assert.assertEquals(ll.head.next.data, 3);
+    Assert.assertEquals(ll.head.next.next.data, 5);
+    Assert.assertEquals(ll.head.next.next.next.data, 7);
+    Assert.assertEquals(ll.head.next.next.next.next.data, 6);
+    Assert.assertEquals(ll.head.next.next.next.next.next.data, 4);
+    Assert.assertEquals(ll.head.next.next.next.next.next.next.data, 2);
+  }
+
+  @Test
+  public void testAlternateMerge() {
+    LinkedList l2 = new LinkedList();
+    l2.insert(10);
+    l2.insert(11);
+
+    ll.mergeAlternate(l2);
+    Assert.assertEquals(ll.head.data, 1);
+    Assert.assertEquals(ll.head.next.data, 10);
+    Assert.assertEquals(ll.head.next.next.data, 2);
+    Assert.assertEquals(ll.head.next.next.next.data, 11);
+    Assert.assertEquals(ll.head.next.next.next.next.data, 3);
+    Assert.assertEquals(ll.head.next.next.next.next.next.data, 4);
+    Assert.assertEquals(ll.head.next.next.next.next.next.next.data, 5);
+    Assert.assertEquals(ll.head.next.next.next.next.next.next.next.data, 6);
+    Assert.assertNull(l2.head);
+  }
+
+  @Test
+  public void testAlternateMerge2() {
+    LinkedList l2 = new LinkedList();
+    l2.insert(10);
+    l2.insert(11);
+    l2.insert(12);
+    l2.insert(13);
+
+    ll.delete(new ListNode(4));
+    ll.delete(new ListNode(5));
+    ll.delete(new ListNode(6));
+
+    ll.mergeAlternate(l2);
+
+    Assert.assertEquals(ll.head.data, 1);
+    Assert.assertEquals(ll.head.next.data, 10);
+    Assert.assertEquals(ll.head.next.next.data, 2);
+    Assert.assertEquals(ll.head.next.next.next.data, 11);
+    Assert.assertEquals(ll.head.next.next.next.next.data, 3);
+    Assert.assertEquals(ll.head.next.next.next.next.next.data, 12);
+    Assert.assertEquals(ll.head.next.next.next.next.next.next.data, 13);
+  }
+
+  @Test
+  public void testAlternateMerge3() {
+    LinkedList l2 = new LinkedList();
+    l2.insert(11);
+    l2.insert(12);
+    l2.insert(13);
+    l2.insert(14);
+    l2.insert(15);
+    l2.insert(16);
+
+    ll.mergeAlternate(l2);
+
+    Assert.assertEquals(ll.head.data, 1);
+    Assert.assertEquals(ll.head.next.data, 11);
+    Assert.assertEquals(ll.head.next.next.data, 2);
+    Assert.assertEquals(ll.head.next.next.next.data, 12);
+    Assert.assertEquals(ll.head.next.next.next.next.data, 3);
+    Assert.assertEquals(ll.head.next.next.next.next.next.data, 13);
+    Assert.assertEquals(ll.head.next.next.next.next.next.next.data, 4);
+    Assert.assertEquals(ll.head.next.next.next.next.next.next.next.data, 14);
+    Assert.assertEquals(ll.head.next.next.next.next.next.next.next.next.data, 5);
+    Assert.assertEquals(ll.head.next.next.next.next.next.next.next.next.next.data, 15);
+    Assert.assertEquals(ll.head.next.next.next.next.next.next.next.next.next.next.data, 6);
+    Assert.assertEquals(ll.head.next.next.next.next.next.next.next.next.next.next.next.data, 16);
+  }
+
+  @Test
+  public void testLoop() {
+    ll.head.next.next.next.next.next.next = ll.head.next;
+    Assert.assertTrue(ll.hasLoop());
+    ll.head.next.next.next.next.next.next = null;
+    Assert.assertFalse(ll.hasLoop());
+  }
+
+  @Test
+  public void removeLoop() {
+    ll.delete(new ListNode(6));
+    ll.head.next.next.next.next.next = ll.head.next.next;
+    ll.removeLoop();
+    Assert.assertEquals(ll.head.data, 1);
+    Assert.assertEquals(ll.head.next.data, 2);
+    Assert.assertEquals(ll.head.next.next.data, 3);
+    Assert.assertEquals(ll.head.next.next.next.data, 4);
+    Assert.assertEquals(ll.head.next.next.next.next.data, 5);
+    Assert.assertNull(ll.head.next.next.next.next.next);
+  }
+
+  @Test
+  public void testIntersection() {
+    LinkedList l2 = new LinkedList();
+    l2.insert(2);
+    l2.insert(3);
+    l2.insert(7);
+    LinkedList result = new LinkedList();
+    ll.intersection(ll.head, l2.head, result);
+    Assert.assertEquals(2, result.head.data);
+    Assert.assertEquals(3, result.head.next.data);
+  }
+
+  @Test
+  public void testUnion() {
+    LinkedList l2 = new LinkedList();
+    l2.insert(1);
+    l2.insert(7);
+    LinkedList result = new LinkedList();
+    ll.union(ll.head, l2.head, result);
+    Assert.assertEquals(1, result.head.data);
+    Assert.assertEquals(2, result.head.next.data);
+    Assert.assertEquals(3, result.head.next.next.data);
+    Assert.assertEquals(4, result.head.next.next.next.data);
+    Assert.assertEquals(5, result.head.next.next.next.next.data);
+    Assert.assertEquals(6, result.head.next.next.next.next.next.data);
+    Assert.assertEquals(7, result.head.next.next.next.next.next.next.data);
+  }
+
+  @Test
+  public void testReverseKElements() {
+    ll.insert(7);
+    ll.insert(8);
+    LinkedList result = new LinkedList();
+    result.head = result.reverseKElements(ll.head, 3);
+    Assert.assertEquals(3, result.head.data);
+    Assert.assertEquals(2, result.head.next.data);
+    Assert.assertEquals(1, result.head.next.next.data);
+    Assert.assertEquals(6, result.head.next.next.next.data);
+    Assert.assertEquals(5, result.head.next.next.next.next.data);
+    Assert.assertEquals(4, result.head.next.next.next.next.next.data);
+    Assert.assertEquals(8, result.head.next.next.next.next.next.next.data);
+    Assert.assertEquals(7, result.head.next.next.next.next.next.next.next.data);
+    Assert.assertNull(result.head.next.next.next.next.next.next.next.next);
+  }
+
+}
